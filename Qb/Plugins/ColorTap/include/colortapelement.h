62fb8057e929ad16b4aa234b9d9f1da463c19d70
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2014-08-20 12:07:44 -0300

    Added ColorTap plugin.

---
+++ Qb/Plugins/ColorTap/include/colortapelement.h
@@ -0,0 +1,56 @@
+/* Webcamoid, webcam capture application.
+ * Copyright (C) 2011-2013  Gonzalo Exequiel Pedone
+ *
+ * Webcamod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Webcamod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Webcamod. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Email     : hipersayan DOT x AT gmail DOT com
+ * Web-Site 1: http://github.com/hipersayanX/Webcamoid
+ * Web-Site 2: http://kde-apps.org/content/show.php/Webcamoid?content=144796
+ */
+
+#ifndef COLORTAPELEMENT_H
+#define COLORTAPELEMENT_H
+
+#include <QImage>
+#include <qrgb.h>
+
+#include <qb.h>
+
+class ColorTapElement: public QbElement
+{
+    Q_OBJECT
+    Q_PROPERTY(QString table READ table WRITE setTable RESET resetTable)
+
+    public:
+        explicit ColorTapElement();
+
+        Q_INVOKABLE QString table() const;
+
+    private:
+        QImage m_table;
+        QString m_tableName;
+
+        QbElementPtr m_convert;
+
+    public slots:
+        void setTable(const QString &table);
+        void resetTable();
+        void iStream(const QbPacket &packet);
+        void setState(QbElement::ElementState state);
+
+    private slots:
+        void processFrame(const QbPacket &packet);
+};
+
+#endif // COLORTAPELEMENT_H
---
+++ Qb/Plugins/ColorTap/src/colortapelement.cpp
@@ -0,0 +1,112 @@
+/* Webcamoid, webcam capture application.
+ * Copyright (C) 2011-2013  Gonzalo Exequiel Pedone
+ *
+ * Webcamod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Webcamod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Webcamod. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Email     : hipersayan DOT x AT gmail DOT com
+ * Web-Site 1: http://github.com/hipersayanX/Webcamoid
+ * Web-Site 2: http://kde-apps.org/content/show.php/Webcamoid?content=144796
+ */
+
+#include "colortapelement.h"
+
+ColorTapElement::ColorTapElement(): QbElement()
+{
+    this->m_convert = Qb::create("VCapsConvert");
+    this->m_convert->setProperty("caps", "video/x-raw,format=bgra");
+
+    QObject::connect(this->m_convert.data(),
+                     SIGNAL(oStream(const QbPacket &)),
+                     this,
+                     SLOT(processFrame(const QbPacket &)));
+
+    this->resetTable();
+}
+
+QString ColorTapElement::table() const
+{
+    return this->m_tableName;
+}
+
+void ColorTapElement::setTable(const QString &table)
+{
+    this->m_tableName = table;
+    this->m_table = QImage(table);
+}
+
+void ColorTapElement::resetTable()
+{
+    this->setTable(":/ColorTap/share/xpro.bmp");
+}
+
+void ColorTapElement::iStream(const QbPacket &packet)
+{
+    if (packet.caps().mimeType() == "video/x-raw")
+        this->m_convert->iStream(packet);
+}
+
+void ColorTapElement::setState(QbElement::ElementState state)
+{
+    QbElement::setState(state);
+    this->m_convert->setState(this->state());
+}
+
+void ColorTapElement::processFrame(const QbPacket &packet)
+{
+    int width = packet.caps().property("width").toInt();
+    int height = packet.caps().property("height").toInt();
+
+    QImage src = QImage((const uchar *) packet.buffer().data(),
+                        width,
+                        height,
+                        QImage::Format_ARGB32);
+
+    int videoArea = width * height;
+
+    QImage oFrame(src.size(), src.format());
+
+    QRgb *srcBits = (QRgb *) src.bits();
+    QRgb *destBits = (QRgb *) oFrame.bits();
+    QRgb *tableBits = (QRgb *) this->m_table.bits();
+
+    for (int i = 0; i < videoArea; i++) {
+        int r = qRed(srcBits[i]);
+        int g = qGreen(srcBits[i]);
+        int b = qBlue(srcBits[i]);
+
+        int ro = qRed(tableBits[r]);
+        int go = qGreen(tableBits[g]);
+        int bo = qBlue(tableBits[b]);
+
+        destBits[i] = qRgb(ro, go, bo);
+    }
+
+    QbBufferPtr oBuffer(new char[oFrame.byteCount()]);
+    memcpy(oBuffer.data(), oFrame.constBits(), oFrame.byteCount());
+
+    QbCaps caps(packet.caps());
+    caps.setProperty("format", "bgra");
+    caps.setProperty("width", oFrame.width());
+    caps.setProperty("height", oFrame.height());
+
+    QbPacket oPacket(caps,
+                     oBuffer,
+                     oFrame.byteCount());
+
+    oPacket.setPts(packet.pts());
+    oPacket.setTimeBase(packet.timeBase());
+    oPacket.setIndex(packet.index());
+
+    emit this->oStream(oPacket);
+}
