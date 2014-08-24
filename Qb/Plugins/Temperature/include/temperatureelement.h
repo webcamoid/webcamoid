3f1926d908b69e2ffb298ae46802b5438fc19385
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2014-08-23 22:53:41 -0300

    Added Temperature plugin.

---
+++ Qb/Plugins/Temperature/include/temperatureelement.h
@@ -0,0 +1,87 @@
+/* Webcamoid, webcam capture application.
+ * Copyright (C) 2011-2013  Gonzalo Exequiel Pedone
+ *
+ * Webcamod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Webcamod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Webcamod. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Email     : hipersayan DOT x AT gmail DOT com
+ * Web-Site 1: http://github.com/hipersayanX/Webcamoid
+ * Web-Site 2: http://kde-apps.org/content/show.php/Webcamoid?content=144796
+ */
+
+#ifndef TEMPERATUREELEMENT_H
+#define TEMPERATUREELEMENT_H
+
+#include <QImage>
+#include <QColor>
+
+#include <qb.h>
+
+class TemperatureElement: public QbElement
+{
+    Q_OBJECT
+    Q_PROPERTY(float temperature READ temperature WRITE setTemperature RESET resetTemperature)
+
+    public:
+        explicit TemperatureElement();
+
+        Q_INVOKABLE float temperature() const;
+
+    private:
+        float m_temperature;
+
+        QbElementPtr m_convert;
+        float m_kr;
+        float m_kg;
+        float m_kb;
+
+        inline void colorFromTemperature(float temperature, float *r, float *g, float *b)
+        {
+            // This algorithm was taken from here:
+            // http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
+
+            // Temperature must fall between 1000 and 40000 degrees
+            temperature = qBound(1000.0, (double) temperature, 40000.0);
+
+            // All calculations require temperature / 100, so only do the conversion once
+            temperature /= 100.0;
+
+            if (temperature <= 66.0)
+                *r = 1;
+            else
+                *r = 1.2929362 * pow(temperature - 60.0, -0.1332047592);
+
+            if (temperature <= 66.0)
+                *g = 0.39008158 * log(temperature) - 0.63184144;
+            else
+                *g = 1.1298909 * pow(temperature - 60, -0.0755148492);
+
+            if (temperature >= 66)
+                *b = 1;
+            else if (temperature <= 19)
+                *b = 0;
+            else
+                *b = 0.54320679 * log(temperature - 10) - 1.1962541;
+        }
+
+    public slots:
+        void setTemperature(float temperature);
+        void resetTemperature();
+        void iStream(const QbPacket &packet);
+        void setState(QbElement::ElementState state);
+
+    private slots:
+        void processFrame(const QbPacket &packet);
+};
+
+#endif // TEMPERATUREELEMENT_H
---
+++ Qb/Plugins/Temperature/src/temperatureelement.cpp
@@ -0,0 +1,115 @@
+/* Webcamoid, webcam capture application.
+ * Copyright (C) 2011-2013  Gonzalo Exequiel Pedone
+ *
+ * Webcamod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Webcamod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Webcamod. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Email     : hipersayan DOT x AT gmail DOT com
+ * Web-Site 1: http://github.com/hipersayanX/Webcamoid
+ * Web-Site 2: http://kde-apps.org/content/show.php/Webcamoid?content=144796
+ */
+
+#include "temperatureelement.h"
+
+TemperatureElement::TemperatureElement(): QbElement()
+{
+    this->m_convert = Qb::create("VCapsConvert");
+    this->m_convert->setProperty("caps", "video/x-raw,format=bgra");
+
+    QObject::connect(this->m_convert.data(),
+                     SIGNAL(oStream(const QbPacket &)),
+                     this,
+                     SLOT(processFrame(const QbPacket &)));
+
+    qRegisterMetaType<QRgb>("QRgb");
+
+    this->resetTemperature();
+}
+
+float TemperatureElement::temperature() const
+{
+    return this->m_temperature;
+}
+
+void TemperatureElement::setTemperature(float temperature)
+{
+    this->m_temperature = temperature;
+
+    this->colorFromTemperature(temperature,
+                               &this->m_kr, &this->m_kg, &this->m_kb);
+}
+
+void TemperatureElement::resetTemperature()
+{
+    this->setTemperature(6500);
+}
+
+void TemperatureElement::iStream(const QbPacket &packet)
+{
+    if (packet.caps().mimeType() == "video/x-raw")
+        this->m_convert->iStream(packet);
+}
+
+void TemperatureElement::setState(QbElement::ElementState state)
+{
+    QbElement::setState(state);
+    this->m_convert->setState(this->state());
+}
+
+void TemperatureElement::processFrame(const QbPacket &packet)
+{
+    int width = packet.caps().property("width").toInt();
+    int height = packet.caps().property("height").toInt();
+
+    QImage src = QImage((const uchar *) packet.buffer().data(),
+                        width,
+                        height,
+                        QImage::Format_ARGB32);
+
+    int videoArea = width * height;
+
+    QImage oFrame(src.size(), src.format());
+
+    QRgb *srcBits = (QRgb *) src.bits();
+    QRgb *destBits = (QRgb *) oFrame.bits();
+
+    for (int i = 0; i < videoArea; i++) {
+        int r = this->m_kr * qRed(srcBits[i]);
+        int g = this->m_kg * qGreen(srcBits[i]);
+        int b = this->m_kb * qBlue(srcBits[i]);
+
+        r = qBound(0, r, 255);
+        g = qBound(0, g, 255);
+        b = qBound(0, b, 255);
+
+        destBits[i] = qRgba(r, g, b, qAlpha(srcBits[i]));
+    }
+
+    QbBufferPtr oBuffer(new char[oFrame.byteCount()]);
+    memcpy(oBuffer.data(), oFrame.constBits(), oFrame.byteCount());
+
+    QbCaps caps(packet.caps());
+    caps.setProperty("format", "bgra");
+    caps.setProperty("width", oFrame.width());
+    caps.setProperty("height", oFrame.height());
+
+    QbPacket oPacket(caps,
+                     oBuffer,
+                     oFrame.byteCount());
+
+    oPacket.setPts(packet.pts());
+    oPacket.setTimeBase(packet.timeBase());
+    oPacket.setIndex(packet.index());
+
+    emit this->oStream(oPacket);
+}
