324bef5b6fcd145e840710c6585549ddf76cefc2
zhcjsjz <zhcj@yeah.net>
2015-04-11 16:13:47 +0200

    Translated using Weblate (Chinese (China))
    Currently translated at 97.5% (78 of 80 strings)

--- share/ts/zh_CN.ts
+++ share/ts/zh_CN.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
@@ -6,47 +6,47 @@
     <message>
         <location filename="../qml/About.qml" line="29"/>
         <source>Add new media</source>
-        <translation type="unfinished"></translation>
+        <translation>添加新媒体</translation>
     </message>
     <message>
         <location filename="../qml/About.qml" line="48"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>信息</translation>
     </message>
     <message>
         <location filename="../qml/About.qml" line="71"/>
         <source>Version %1</source>
-        <translation type="unfinished"></translation>
+        <translation>版本%1</translation>
     </message>
     <message>
         <location filename="../qml/About.qml" line="76"/>
         <source>Using Qt %1</source>
-        <translation type="unfinished"></translation>
+        <translation>使用Qt %1</translation>
     </message>
     <message>
         <location filename="../qml/About.qml" line="90"/>
         <source>Webcam capture application.</source>
-        <translation type="unfinished"></translation>
+        <translation>摄像头捕捉软件</translation>
     </message>
     <message>
         <location filename="../qml/About.qml" line="93"/>
         <source>A simple webcam application for picture and video capture.</source>
-        <translation type="unfinished"></translation>
+        <translation>一个简单的抓图和录制视频的摄像头软件</translation>
     </message>
     <message>
         <location filename="../qml/About.qml" line="82"/>
         <source>Website</source>
-        <translation type="unfinished"></translation>
+        <translation>网站</translation>
     </message>
     <message>
         <location filename="../qml/About.qml" line="107"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>许可</translation>
     </message>
     <message>
         <location filename="../qml/About.qml" line="118"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>关闭</translation>
     </message>
 </context>
 <context>
@@ -54,47 +54,47 @@
     <message>
         <location filename="../qml/AddMedia.qml" line="30"/>
         <source>Add new media</source>
-        <translation type="unfinished"></translation>
+        <translation>添加新媒体</translation>
     </message>
     <message>
         <location filename="../qml/AddMedia.qml" line="67"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>描述</translation>
     </message>
     <message>
         <location filename="../qml/AddMedia.qml" line="74"/>
         <source>Insert media description</source>
-        <translation type="unfinished"></translation>
+        <translation>插入媒体描述</translation>
     </message>
     <message>
         <location filename="../qml/AddMedia.qml" line="81"/>
         <source>Media file</source>
-        <translation type="unfinished"></translation>
+        <translation>媒体文件</translation>
     </message>
     <message>
         <location filename="../qml/AddMedia.qml" line="89"/>
         <source>Select media file</source>
-        <translation type="unfinished"></translation>
+        <translation>选择媒体文件</translation>
     </message>
     <message>
         <location filename="../qml/AddMedia.qml" line="96"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索</translation>
     </message>
     <message>
         <location filename="../qml/AddMedia.qml" line="118"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>确定</translation>
     </message>
     <message>
         <location filename="../qml/AddMedia.qml" line="141"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>取消</translation>
     </message>
     <message>
         <location filename="../qml/AddMedia.qml" line="152"/>
         <source>Choose the file to add as media</source>
-        <translation type="unfinished"></translation>
+        <translation>选择一个文件作为媒体</translation>
     </message>
 </context>
 <context>
@@ -102,47 +102,47 @@
     <message>
         <location filename="../qml/AddRecordingFormat.qml" line="30"/>
         <source>Add new recording format</source>
-        <translation type="unfinished"></translation>
+        <translation>添加新记录格式</translation>
     </message>
     <message>
         <location filename="../qml/AddRecordingFormat.qml" line="61"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>描述</translation>
     </message>
     <message>
         <location filename="../qml/AddRecordingFormat.qml" line="67"/>
         <source>Insert recording format description</source>
-        <translation type="unfinished"></translation>
+        <translation>插入记录格式描述</translation>
     </message>
     <message>
         <location filename="../qml/AddRecordingFormat.qml" line="73"/>
         <source>Suffix</source>
-        <translation type="unfinished"></translation>
+        <translation>后缀</translation>
     </message>
     <message>
         <location filename="../qml/AddRecordingFormat.qml" line="79"/>
         <source>Insert wanted output file suffix</source>
-        <translation type="unfinished"></translation>
+        <translation>插入想要输出的文件后缀</translation>
     </message>
     <message>
         <location filename="../qml/AddRecordingFormat.qml" line="86"/>
         <source>Parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>参数</translation>
     </message>
     <message>
         <location filename="../qml/AddRecordingFormat.qml" line="92"/>
         <source>Insert encoding parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>插入编码参数</translation>
     </message>
     <message>
         <location filename="../qml/AddRecordingFormat.qml" line="109"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>确定</translation>
     </message>
     <message>
         <location filename="../qml/AddRecordingFormat.qml" line="128"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>取消</translation>
     </message>
 </context>
 <context>
@@ -150,12 +150,12 @@
     <message>
         <location filename="../qml/EffectBar.qml" line="125"/>
         <source>Search effect...</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索效果</translation>
     </message>
     <message>
         <location filename="../qml/EffectBar.qml" line="171"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>无</translation>
     </message>
 </context>
 <context>
@@ -164,27 +164,27 @@
         <location filename="../qml/EffectConfig.qml" line="42"/>
         <location filename="../qml/EffectConfig.qml" line="49"/>
         <source>Plugin id</source>
-        <translation type="unfinished"></translation>
+        <translation>插件id</translation>
     </message>
     <message>
         <location filename="../qml/EffectConfig.qml" line="55"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>描述</translation>
     </message>
     <message>
         <location filename="../qml/EffectConfig.qml" line="62"/>
         <source>Plugin description</source>
-        <translation type="unfinished"></translation>
+        <translation>插件描述</translation>
     </message>
     <message>
         <location filename="../qml/EffectConfig.qml" line="77"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>删除</translation>
     </message>
     <message>
         <location filename="../qml/EffectConfig.qml" line="77"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>添加</translation>
     </message>
 </context>
 <context>
@@ -192,27 +192,27 @@
     <message>
         <location filename="../qml/GeneralConfig.qml" line="40"/>
         <source>Record audio from</source>
-        <translation type="unfinished"></translation>
+        <translation>录制音频从</translation>
     </message>
     <message>
         <location filename="../qml/GeneralConfig.qml" line="48"/>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>源</translation>
     </message>
     <message>
         <location filename="../qml/GeneralConfig.qml" line="68"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>无</translation>
     </message>
     <message>
         <location filename="../qml/GeneralConfig.qml" line="79"/>
         <source>Play audio from source</source>
-        <translation type="unfinished"></translation>
+        <translation>从源播放音频</translation>
     </message>
     <message>
         <location filename="../qml/GeneralConfig.qml" line="58"/>
         <source>Mic</source>
-        <translation type="unfinished"></translation>
+        <translation>麦克风</translation>
     </message>
 </context>
 <context>
@@ -220,32 +220,32 @@
     <message>
         <location filename="../qml/MediaConfig.qml" line="54"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>描述</translation>
     </message>
     <message>
         <location filename="../qml/MediaConfig.qml" line="61"/>
         <source>Insert media description</source>
-        <translation type="unfinished"></translation>
+        <translation>插件媒体描述</translation>
     </message>
     <message>
         <location filename="../qml/MediaConfig.qml" line="68"/>
         <source>Media file</source>
-        <translation type="unfinished"></translation>
+        <translation>媒体文件</translation>
     </message>
     <message>
         <location filename="../qml/MediaConfig.qml" line="75"/>
         <source>Select media file</source>
-        <translation type="unfinished"></translation>
+        <translation>选择媒体文件</translation>
     </message>
     <message>
         <location filename="../qml/MediaConfig.qml" line="90"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>编辑</translation>
     </message>
     <message>
         <location filename="../qml/MediaConfig.qml" line="99"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>删除</translation>
     </message>
 </context>
 <context>
@@ -253,63 +253,63 @@
     <message>
         <location filename="../qml/RecordConfig.qml" line="72"/>
         <source>Video %1.%2</source>
-        <translation type="unfinished"></translation>
+        <translation>视频%1.%2</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="82"/>
         <source>Stop recording video</source>
-        <translation type="unfinished"></translation>
+        <translation>停止录制视频</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="86"/>
         <location filename="../qml/RecordConfig.qml" line="164"/>
         <source>Start recording video</source>
-        <translation type="unfinished"></translation>
+        <translation>开始录制视频</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="93"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>描述</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="100"/>
         <source>Insert format description</source>
-        <translation type="unfinished"></translation>
+        <translation>插入格式描述</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="106"/>
         <source>Suffix</source>
-        <translation type="unfinished"></translation>
+        <translation>后缀</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="113"/>
         <source>Supported file suffix</source>
-        <translation type="unfinished"></translation>
+        <translation>支持的文件后缀</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="119"/>
         <source>Parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>参数</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="126"/>
         <source>Encoding parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>编码参数</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="141"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>编辑</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="150"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>删除</translation>
     </message>
     <message>
         <location filename="../qml/RecordConfig.qml" line="228"/>
         <source>Save video as...</source>
-        <translation type="unfinished"></translation>
+        <translation>保存视频为...</translation>
     </message>
 </context>
 <context>
@@ -317,67 +317,67 @@
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="28"/>
         <source>Warning, Caps Lock is ON!</source>
-        <translation type="unfinished"></translation>
+        <translation>注意，大写锁定键为开！</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="29"/>
         <source>Layout</source>
-        <translation type="unfinished"></translation>
+        <translation>布局</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="30"/>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>登录</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="31"/>
         <source>Login failed</source>
-        <translation type="unfinished"></translation>
+        <translation>登录失败</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="32"/>
         <source>Login succeeded</source>
-        <translation type="unfinished"></translation>
+        <translation>登录成功</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="33"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>密码</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="34"/>
         <source>Enter your username and password</source>
-        <translation type="unfinished"></translation>
+        <translation>输入你的用户名和密码</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="35"/>
         <source>Select your user and enter password</source>
-        <translation type="unfinished"></translation>
+        <translation>选择你的用户名并输入密码</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="36"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>重启</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="37"/>
         <source>Session</source>
-        <translation type="unfinished"></translation>
+        <translation>会话</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="38"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation>关机</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="39"/>
         <source>User name</source>
-        <translation type="unfinished"></translation>
+        <translation>用户名</translation>
     </message>
     <message>
         <location filename="../../../../../../../../usr/lib/qt/qml/SddmComponents/TextConstants.qml" line="40"/>
         <source>Welcome to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>欢迎来到%1</translation>
     </message>
 </context>
 <context>
@@ -385,52 +385,52 @@
     <message>
         <location filename="../qml/main.qml" line="106"/>
         <source>Recording</source>
-        <translation type="unfinished"></translation>
+        <translation>录制中</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="328"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>播放</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="341"/>
         <source>Configure streams</source>
-        <translation type="unfinished"></translation>
+        <translation>视频配置</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="354"/>
         <source>Take a photo</source>
-        <translation type="unfinished"></translation>
+        <translation>拍照</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="361"/>
         <source>Picture %1.%2</source>
-        <translation type="unfinished"></translation>
+        <translation>照片%1.%2</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="383"/>
         <source>Record video</source>
-        <translation type="unfinished"></translation>
+        <translation>录制视频</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="397"/>
         <source>Configure Effects</source>
-        <translation type="unfinished"></translation>
+        <translation>效果配置</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="410"/>
         <source>Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>配置</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="423"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../qml/main.qml" line="370"/>
         <source>Save photo as...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
