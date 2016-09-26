15fd591c6c44df5eb27c802f2d0542651a9985f9
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2016-09-26 00:04:06 -0300

    pkg-config build may work now in Mac.

--- ports/homebrew/webcamoid.rb
+++ ports/homebrew/webcamoid.rb
@@ -5,6 +5,7 @@ class Webcamoid < Formula
     version "8.0.0"
     sha256 "b8ee97a40a3d6c6a522b5e544c67af2dacae0a606b341716cacc62a21d90b68b"
 
+    depends_on "pkg-config" => :build
     depends_on "qt5"
     depends_on "ffmpeg"
     depends_on "libuvc" => :optional
@@ -16,21 +17,11 @@ class Webcamoid < Formula
             spec = "macx-g++"
         end
 
-        ffver = `ffmpeg -version | head -n 1 | awk '{printf "%s", $3}'`
-
         args = %W[
             -config release
             -spec #{spec}
             CONFIG-=app_bundle
             PREFIX=#{prefix}
-            FFMPEGINCLUDES="/usr/local/Cellar/ffmpeg/#{ffver}/include"
-            FFMPEGLIBS=-L"/usr/local/Cellar/ffmpeg/#{ffver}/lib"
-            FFMPEGLIBS+=-lavcodec
-            FFMPEGLIBS+=-lavdevice
-            FFMPEGLIBS+=-lavformat
-            FFMPEGLIBS+=-lavutil
-            FFMPEGLIBS+=-lswresample
-            FFMPEGLIBS+=-lswscale
         ]
 
         system "qmake", "Webcamoid.pro", *args
