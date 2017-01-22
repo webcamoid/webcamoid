764d41a1e08176932a3f7f8df44c9ddd7aaa444f
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2017-01-22 00:38:21 -0300

    Some fixes for the Travis build 3.

--- ports/ci/travis/build.sh
+++ ports/ci/travis/build.sh
@@ -12,20 +12,17 @@ fi
 
 if [ "${TRAVIS_OS_NAME}" = linux ]; then
     if [ "${DOCKERSYS}" = debian ]; then
-        ${EXEC} qmake -qt=5 Webcamoid.pro \
-            -spec ${COMPILESPEC} \
+        ${EXEC} qmake -qt=5 -spec ${COMPILESPEC} Webcamoid.pro \
             QMAKE_CXX="${COMPILER}"
     else
-        ${EXEC} qmake-qt5 Webcamoid.pro \
-            -spec ${COMPILESPEC} \
+        ${EXEC} qmake-qt5 -spec ${COMPILESPEC} Webcamoid.pro \
             QMAKE_CXX="${COMPILER}"
     fi
 elif [ "${TRAVIS_OS_NAME}" = osx ]; then
     LIBUSBVER=$(ls /usr/local/Cellar/libusb | tail -n 1)
     LIBUVCVER=$(ls /usr/local/Cellar/libuvc | tail -n 1)
 
-    ${EXEC} qmake Webcamoid.pro \
-        -spec ${COMPILESPEC} \
+    ${EXEC} qmake -spec ${COMPILESPEC} Webcamoid.pro \
         QMAKE_CXX="${COMPILER}" \
         LIBUSBINCLUDES=/usr/local/Cellar/libusb/${LIBUSBVER}/include \
         LIBUVCINCLUDES=/usr/local/Cellar/libuvc/${LIBUVCVER}/include \
