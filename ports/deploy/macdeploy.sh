2c1804e8f22f6b41d6f567fc67d41a8c24030aec
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2017-02-23 18:51:08 -0300

    Mode fixes for Mac deployment 2.

--- ports/deploy/macdeploy.sh
+++ ports/deploy/macdeploy.sh
@@ -1,31 +1,97 @@
 #!/bin/sh
 
+CURPATH=$(dirname $0)
 QTVER=$(ls /usr/local/Cellar/qt5 | tail -n 1)
 QT5PATH=/usr/local/Cellar/qt5/${QTVER}
+QT5OPT=/usr/local/opt/qt5
 
-${QT5PATH}/bin/macdeployqt \
-    ../../StandAlone/webcamoid.app \
-    -always-overwrite \
-    -appstore-compliant \
-    -qmldir=../../libAvKys \
-    -libpath=../../libAvKys/Lib
+function deploy {
+    echo Deploying app...
 
-# Fix QtCore
-install_name_tool -id \
-    @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore \
-    ../../StandAlone/webcamoid.app/Contents/Frameworks/QtCore.framework/Versions/5/QtCore
+    ${QT5PATH}/bin/macdeployqt \
+        ${CURPATH}/../../StandAlone/webcamoid.app \
+        -always-overwrite \
+        -appstore-compliant \
+        -libpath=${CURPATH}/../../libAvKys/Lib
+}
 
-install_name_tool -change \
-    ${QT5PATH}/lib/QtCore.framework/Versions/5/QtCore \
-    @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore \
-    ../../StandAlone/webcamoid.app/Contents/MacOs/webcamoid
+function installplugins {
+    pushd ${CURPATH}/../../libAvkys
+        make INSTALL_ROOT="${PWD}/bundle-data" install
+        cp -Rvf ${PWD}/bundle-data/usr/lib/qt/qml/AkQml \
+                ${PWD}/../StandAlone/webcamoid.app/Contents/Resources/qml
+        cp -Rvf ${PWD}/bundle-data/usr/lib/avkys \
+                ${PWD}/../StandAlone/webcamoid.app/Contents/Plugins
+        rm -Rvf ${PWD}/bundle-data
+    popd
+}
 
-# Fix QtGui
-install_name_tool -id \
-    @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui \
-    ../../StandAlone/webcamoid.app/Contents/Frameworks/QtGui.framework/Versions/5/QtGui
+function fixlibs {
+    path=$1
 
-install_name_tool -change \
-    ${QT5PATH}/lib/QtGui.framework/Versions/5/QtGui \
-    @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui \
-    ../../StandAlone/webcamoid.app/Contents/MacOs/webcamoid
+    for libpath in $(find ${path} -name '*.dylib' \
+                                  -or \
+                                  -name '*.framework' \
+                                  -or \
+                                  -name 'webcamoid'); do
+        libpath=${libpath/${path}\//}
+        fname=$(basename $libpath)
+        echo Fixing $libpath
+
+        if [[ $libpath == *.dylib || $libpath == webcamoid ]]; then
+            where=${path}/$libpath
+        else
+            module=${fname%.framework}
+            where=${path}/$libpath/$module
+        fi
+
+        otool -L $where | \
+        while read lib; do
+            if [[ "$lib" == *:*
+                  || "$lib" == /usr/lib*
+                  || "$lib" == /System/Library/Frameworks/* ]]; then
+                continue
+            fi
+
+            oldpath=${lib%(*}
+            relpath=@executable_path/../Frameworks
+            changeid=0
+            echo '    dep ' $oldpath
+
+            if [[ "$lib" == $QT5PATH\/lib* ]]; then
+                newpath=${oldpath/$QT5PATH\/lib/$relpath}
+                echo '          change path to' $newpath
+            elif [[ "$lib" == @executable_path/../Frameworks/*$fname* ]]; then
+                newpath=${oldpath/@executable_path\/..\/Frameworks/$relpath}
+                changeid=1
+                echo '          change id to ' $newpath
+            elif [[ "$lib" == @executable_path/../Frameworks* ]]; then
+                newpath=${oldpath/@executable_path\/..\/Frameworks/$relpath}
+                echo '          change path to' $newpath
+            elif [[ "$lib" == $QT5OPT* ]]; then
+                newpath=$(basename $oldpath)
+                changeid=1
+                echo '          change id to' $newpath
+            else
+                continue
+            fi
+
+            if [ $changeid -eq 0 ]; then
+                install_name_tool -change \
+                    $oldpath \
+                    $newpath \
+                    $where
+            else
+                install_name_tool -id \
+                    $newpath \
+                    $where
+            fi
+        done
+    done
+}
+
+deploy
+installplugins
+fixlibs ${CURPATH}/../../StandAlone/webcamoid.app/Contents/MacOS
+fixlibs ${CURPATH}/../../StandAlone/webcamoid.app/Contents/Frameworks
+fixlibs ${CURPATH}/../../StandAlone/webcamoid.app/Contents/Plugins
