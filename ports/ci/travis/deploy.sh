9b92bc9a2fc4cd491092eae69b3b138d4acb622f
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2017-08-30 16:23:07 -0300

    Some fixes for deploy scripts 2.

---
+++ ports/ci/travis/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "${TRAVIS_OS_NAME}" = linux ] && [ -z "${ANDROID_BUILD}" ]; then
+    EXEC="docker exec ${DOCKERSYS}"
+fi
+
+BUILDSCRIPT=dockerbuild.sh
+
+if [ "${ANDROID_BUILD}" = 1 ]; then
+    echo "Deploy not supported for Android"
+elif [ "${TRAVIS_OS_NAME}" = linux ]; then
+    ${EXEC} python3 ports/deploy/deploy.py
+elif [ "${TRAVIS_OS_NAME}" = osx ]; then
+    ${EXEC} python3 ports/deploy/deploy.py
+fi
