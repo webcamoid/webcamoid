445a230d292b7bb957a6c342e1de23f14de45b04
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2018-12-17 15:14:25 -0300

    Added CopyLeft notice to CI scripts.

--- ports/ci/appveyor/deploy.bat
+++ ports/ci/appveyor/deploy.bat
@@ -1,2 +1,20 @@
+REM Webcamoid, webcam capture application.
+REM Copyright (C) 2017  Gonzalo Exequiel Pedone
+REM
+REM Webcamoid is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation, either version 3 of the License, or
+REM (at your option) any later version.
+REM
+REM Webcamoid is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM
+REM You should have received a copy of the GNU General Public License
+REM along with Webcamoid. If not, see <http://www.gnu.org/licenses/>.
+REM
+REM Web-Site: http://webcamoid.github.io/
+
 set MAKE_PATH=%TOOLSDIR%\bin\%MAKETOOL%.exe
 C:\Python36\python.exe ports\deploy\deploy.py
