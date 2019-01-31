ac5123596ef4b5867d8efd4295e841df9aafd03b
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2019-01-31 20:23:38 -0300

    Fix AppVeyor script 14.

--- ports/ci/appveyor/upload.bat
+++ ports/ci/appveyor/upload.bat
@@ -1,6 +1,23 @@
 if not "%DAILY_BUILD%" == "" (
-    curl ^
-        -T ports\deploy\packages_auto\windows\* ^
-        -uhipersayanx:%BT_KEY% ^
-        https://api.bintray.com/content/webcamoid/webcamoid/webcamoid/daily/windows
+rem      curl ^
+rem          -T ports\deploy\packages_auto\windows\* ^
+rem          -uhipersayanx:%BT_KEY% ^
+rem          https://api.bintray.com/content/webcamoid/webcamoid/webcamoid/daily/windows
+
+    jfrog bt upload ^
+        --user hipersayanx ^
+        --key %BT_KEY% ^
+        --override true ^
+        --publish true ^
+        --licenses GPLv3+ ^
+        --vcs-url "https://github.com/webcamoid/webcamoid.git" ^
+        --pub-dn true ^
+        --desc "Daily build" ^
+        --labels daily-build ^
+        --website-url "https://webcamoid.github.io" ^
+        --issuetracker-url "https://github.com/webcamoid/webcamoid/issues" ^
+        --github-repo "https://github.com/webcamoid/webcamoid" ^
+        ports\deploy\packages_auto\windows\* ^
+        webcamoid/webcamoid/webcamoid/daily ^
+        windows/
 )
