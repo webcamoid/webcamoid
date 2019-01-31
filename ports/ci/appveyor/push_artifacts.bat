2f263c5c381aa37d41063152f85dc8188a2a1c19
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2019-01-31 19:22:13 -0300

    Fix AppVeyor script 12.

--- ports/ci/appveyor/push_artifacts.bat
+++ ports/ci/appveyor/push_artifacts.bat
@@ -1,7 +1,5 @@
-set PACKAGES_PATH=ports\deploy\packages_auto\windows
-
 if not "%DAILY_BUILD%" == "" (
-    for %%f in (%PACKAGES_PATH%\*) do (
-        appveyor PushArtifact %PACKAGES_PATH%\%%f
+    for %%f in (ports\deploy\packages_auto\windows\*) do (
+        appveyor PushArtifact %%f
     )
 )
