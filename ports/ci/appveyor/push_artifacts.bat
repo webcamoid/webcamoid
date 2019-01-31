169de44a10fbb660317b2bce2683e5206261a961
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2019-01-31 16:32:34 -0300

    Fix AppVeyor script 7.

--- ports/ci/appveyor/push_artifacts.bat
+++ ports/ci/appveyor/push_artifacts.bat
@@ -1,5 +1,8 @@
 dir
 
 if not "%DAILY_BUILD%" == "" (
-    appveyor PushArtifact ports\deploy\packages_auto\windows\*
+    appveyor PushArtifact ports\deploy\packages_auto\windows\webcamoid-portable-8.5.0-win32.zip
+    appveyor PushArtifact ports\deploy\packages_auto\windows\webcamoid-8.5.0-win32.exe
 )
+
+dir
