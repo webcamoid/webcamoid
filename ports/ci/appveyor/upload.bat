9e954139ef2b5ac3032c6459b62225710fc3f66f
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2019-01-31 21:11:57 -0300

    Fix AppVeyor script 17.

--- ports/ci/appveyor/upload.bat
+++ ports/ci/appveyor/upload.bat
@@ -4,20 +4,22 @@ rem          -T ports\deploy\packages_auto\windows\* ^
 rem          -uhipersayanx:%BT_KEY% ^
 rem          https://api.bintray.com/content/webcamoid/webcamoid/webcamoid/daily/windows
 
-    jfrog bt upload ^
-        --user hipersayanx ^
-        --key %BT_KEY% ^
-        --override true ^
-        --publish true ^
-        --licenses GPLv3+ ^
-        --vcs-url "https://github.com/webcamoid/webcamoid.git" ^
-        --pub-dn true ^
-        --desc "Daily build" ^
-        --labels daily-build ^
-        --website-url "https://webcamoid.github.io" ^
-        --issuetracker-url "https://github.com/webcamoid/webcamoid/issues" ^
-        --github-repo "https://github.com/webcamoid/webcamoid" ^
-        ports/deploy/packages_auto/windows/* ^
-        webcamoid/webcamoid/webcamoid/daily ^
-        windows/
+    for %%f in (ports\deploy\packages_auto\windows\*) do (
+        jfrog bt upload ^
+            --user hipersayanx ^
+            --key %BT_KEY% ^
+            --override true ^
+            --publish true ^
+            --licenses GPLv3+ ^
+            --vcs-url "https://github.com/webcamoid/webcamoid.git" ^
+            --pub-dn true ^
+            --desc "Daily build" ^
+            --labels daily-build ^
+            --website-url "https://webcamoid.github.io" ^
+            --issuetracker-url "https://github.com/webcamoid/webcamoid/issues" ^
+            --github-repo "https://github.com/webcamoid/webcamoid" ^
+            %%f ^
+            webcamoid/webcamoid/webcamoid/daily ^
+            windows/
+    )
 )
