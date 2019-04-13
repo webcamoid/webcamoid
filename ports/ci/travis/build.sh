c3b108d0926ba4dc2e2e9d1d326d8fdaa34c491b
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2019-04-13 10:38:28 -0300

    Fix Travis build 3.

--- ports/ci/travis/build.sh
+++ ports/ci/travis/build.sh
@@ -52,13 +52,13 @@ fi
 
 if [ "${ANDROID_BUILD}" = 1 ]; then
     export PATH=$PWD/build/Qt/${QTVER}/android_${TARGET_ARCH}/bin:$PATH
+    #export ANDROID_HOME=/usr/local/android-sdk
+    export ANDROID_NDK=$PWD/build/android-ndk-${NDKVER}
+    export ANDROID_NDK_HOME=${ANDROID_NDK}
+    export ANDROID_NDK_PLATFORM=android-${ANDROID_MINIMUM_PLATFORM}
+    export ANDROID_NDK_ROOT=${ANDROID_NDK}
+    export ANDROID_SDK_ROOT=${ANDROID_HOME}
     env | grep ANDROID_
-#     export ANDROID_HOME=/opt/android-sdk
-#     export ANDROID_NDK=/opt/android-ndk
-#     export ANDROID_NDK_HOME=/opt/android-ndk
-#     export ANDROID_NDK_PLATFORM=android-22
-     export ANDROID_NDK_ROOT=$PWD/build/android-ndk-${NDKVER}
-#     export ANDROID_SDK_ROOT=/opt/android-sdk
     qmake -query
     qmake -spec ${COMPILESPEC} Webcamoid.pro \
         CONFIG+=silent
