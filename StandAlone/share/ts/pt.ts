58012e6d9f2832edd7f6b50e25c2e6477b5ed8f6
ssantos <ssantos@web.de>
2019-09-30 17:10:15 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (402 of 402 strings)

--- StandAlone/share/ts/pt.ts
+++ StandAlone/share/ts/pt.ts
@@ -2158,7 +2158,7 @@
     <message>
         <location filename="../../../libAvKys/Plugins/VirtualCamera/share/qml/main.qml" line="264"/>
         <source>Error creating camera: </source>
-        <translation type="unfinished"></translation>
+        <translation>Erro ao criar a câmera: </translation>
     </message>
     <message>
         <location filename="../../../libAvKys/Plugins/VirtualCamera/share/qml/main.qml" line="152"/>
