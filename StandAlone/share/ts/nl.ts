a9c23e1525826b55b5deb9e889592ff0722416e0
Heimen Stoffels <vistausss@outlook.com>
2020-03-04 19:03:37 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (411 of 411 strings)

--- StandAlone/share/ts/nl.ts
+++ StandAlone/share/ts/nl.ts
@@ -670,22 +670,22 @@
     <message>
         <location filename="../qml/SettingsMenu.qml" line="26"/>
         <source>Video</source>
-        <translation type="unfinished">Video</translation>
+        <translation>Video</translation>
     </message>
     <message>
         <location filename="../qml/SettingsMenu.qml" line="30"/>
         <source>Audio</source>
-        <translation type="unfinished">Audio</translation>
+        <translation>Audio</translation>
     </message>
     <message>
         <location filename="../qml/SettingsMenu.qml" line="34"/>
         <source>Preferences</source>
-        <translation type="unfinished">Instellingen</translation>
+        <translation>Instellingen</translation>
     </message>
     <message>
         <location filename="../qml/SettingsMenu.qml" line="39"/>
         <source>Play sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Bronnen afspelen</translation>
     </message>
 </context>
 <context>
@@ -2261,12 +2261,12 @@
     <message>
         <location filename="../qml/main.qml" line="149"/>
         <source>Now</source>
-        <translation type="unfinished">Zojuist</translation>
+        <translation>Zojuist</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="156"/>
         <source>%1 seconds</source>
-        <translation type="unfinished">%1 seconden</translation>
+        <translation>%1 seconden</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="550"/>
@@ -2281,12 +2281,12 @@
     <message>
         <location filename="../qml/main.qml" line="687"/>
         <source>Save photo as…</source>
-        <translation type="unfinished">Foto opslaan als…</translation>
+        <translation>Foto opslaan als…</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="689"/>
         <source>Picture %1.png</source>
-        <translation type="unfinished">Afbeelding %1.png</translation>
+        <translation>Afbeelding %1.png</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="382"/>
