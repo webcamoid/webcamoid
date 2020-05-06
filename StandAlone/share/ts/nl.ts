1d00615fc30c3733f05a68b5b13c4123c5d736d8
Heimen Stoffels <vistausss@outlook.com>
2020-05-06 08:57:52 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (402 of 402 strings)

--- StandAlone/share/ts/nl.ts
+++ StandAlone/share/ts/nl.ts
@@ -248,7 +248,7 @@
     <message>
         <location filename="../qml/AudioVideoPanel.qml" line="32"/>
         <source>Video Input Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Video-invoeropties</translation>
     </message>
 </context>
 <context>
@@ -737,12 +737,12 @@
     <message>
         <location filename="../qml/VideoInputOptions.qml" line="59"/>
         <source>Edit</source>
-        <translation type="unfinished">Bewerken</translation>
+        <translation>Bewerken</translation>
     </message>
     <message>
         <location filename="../qml/VideoInputOptions.qml" line="71"/>
         <source>Remove</source>
-        <translation type="unfinished">Verwijderen</translation>
+        <translation>Verwijderen</translation>
     </message>
 </context>
 <context>
@@ -755,7 +755,7 @@
     <message>
         <location filename="../qml/VideoInputs.qml" line="59"/>
         <source>No webcams found</source>
-        <translation type="unfinished">Geen webcams aangetroffen</translation>
+        <translation>Geen webcams aangetroffen</translation>
     </message>
 </context>
 <context>
@@ -2247,7 +2247,7 @@
     <message>
         <location filename="../qml/main.qml" line="185"/>
         <source>Use flash</source>
-        <translation type="unfinished"></translation>
+        <translation>Flitser gebruiken</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="205"/>
