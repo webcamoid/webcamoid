aaf68e25426d29c42add8416f2d99e5739722b11
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2021-07-24 18:22:35 -0300

    Fixed build in Linux MinGW.

--- ports/ci/linux-mingw/build.sh
+++ ports/ci/linux-mingw/build.sh
@@ -46,5 +46,5 @@ ${TARGET_ARCH}-w64-mingw32-cmake \
     ${EXTRA_PARAMS} \
     -DDAILY_BUILD=${DAILY_BUILD}
 ${TARGET_ARCH}-w64-mingw32-cmake -LA -S . -B ${buildDir}
-make -C ${buildDir} -j${NJOBS}
-make -C ${buildDir} install
+${TARGET_ARCH}-w64-mingw32-cmake --build ${buildDir} --parallel ${NJOBS}
+${TARGET_ARCH}-w64-mingw32-cmake --install ${buildDir}
