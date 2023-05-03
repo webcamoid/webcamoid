6ad8b4c41357c45d2390775975dafce5c888fa01
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2023-05-03 15:15:59 -0300

    Build GNU/Linux ARM packages in Cirrus CI.

---
+++ ports/ci/linux-ubuntu/upload.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Webcamoid, webcam capture application.
+# Copyright (C) 2023  Gonzalo Exequiel Pedone
+#
+# Webcamoid is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Webcamoid is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Webcamoid. If not, see <http://www.gnu.org/licenses/>.
+#
+# Web-Site: http://webcamoid.github.io/
+
+if [[ "${UPLOAD}" != 1 ]]; then
+    exit 0
+fi
+
+apt-get -qq -y install gh
+
+if [[ "$CIRRUS_RELEASE" == "" ]]; then
+    releaseName=daily-build
+else
+    releaseName=$CIRRUS_RELEASE
+fi
+
+gh release upload "$releaseName" webcamoid-packages/linux-jammy-gcc/* --clobber -R "$CIRRUS_REPO_FULL_NAME"
