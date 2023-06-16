ff9548d74d66ea715664c4539e5f39123c6bc363
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2023-06-16 13:48:53 -0300

    Added build scripts for F-Droid.

---
+++ ports/ci/fdroid/deploy.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Webcamoid, webcam capture application.
+# Copyright (C) 2023  Gonzalo Exequiel Pedone
+#
+# Webcamoid is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Webcamoid is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Webcamoid. If not, see <http://www.gnu.org/licenses/>.
+#
+# Web-Site: http://webcamoid.github.io/
+
+git clone https://github.com/webcamoid/DeployTools.git
+
+export PYTHONPATH="${PWD}/DeployTools"
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed 's:bin/java::')
+export ANDROID_HOME="${SDK}"
+export ANDROID_NDK="${NDK}"
+export ANDROID_NDK_HOME=${ANDROID_NDK}
+export ANDROID_NDK_HOST=linux-x86_64
+export ANDROID_NDK_PLATFORM=android-${ANDROID_PLATFORM}
+export ANDROID_NDK_ROOT=${ANDROID_NDK}
+export ANDROID_SDK_ROOT=${ANDROID_HOME}
+export PATH="${JAVA_HOME}/bin/java:${PATH}"
+export PATH="$PATH:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin"
+export PATH="${PATH}:${ANDROID_HOME}/platform-tools"
+export PATH="${PATH}:${ANDROID_HOME}/emulator"
+export PATH="${PATH}:${ANDROID_NDK}"
+export ORIG_PATH="${PATH}"
+export KEYSTORE_PATH="${PWD}/keystores/debug.keystore"
+nArchs=$(echo "${TARGET_ARCH}" | tr ':' ' ' | wc -w)
+lastArch=$(echo "${TARGET_ARCH}" | awk -F: '{print $NF}')
+
+cat << EOF > package_info_sdkbt.conf
+[System]
+sdkBuildToolsRevision = ${ANDROID_BUILD_TOOLS_VERSION}
+EOF
+
+if [ "${nArchs}" = 1 ]; then
+    export PATH="${PWD}/build/Qt/${QTVER_ANDROID}/android/bin:${PWD}/.local/bin:${ORIG_PATH}"
+    export PACKAGES_DIR=${PWD}/webcamoid-packages/android-${lastArch}
+    export BUILD_PATH=${PWD}/build-${lastArch}
+
+    python3 DeployTools/deploy.py \
+        -d "${BUILD_PATH}/android-build" \
+        -c "${BUILD_PATH}/package_info.conf" \
+        -c "${BUILD_PATH}/package_info_android.conf" \
+        -c "${PWD}/package_info_sdkbt.conf" \
+        -o "${PACKAGES_DIR}"
+else
+    export PACKAGES_DIR=${PWD}/webcamoid-packages/android
+    mkdir -p "${PWD}/webcamoid-data"
+
+    for arch_ in $(echo "${TARGET_ARCH}" | tr ":" "\n"); do
+        export PATH="${PWD}/build/Qt/${QTVER_ANDROID}/android/bin:${PWD}/.local/bin:${ORIG_PATH}"
+        export BUILD_PATH=${PWD}/build-${arch_}
+
+        python3 DeployTools/deploy.py \
+            -r \
+            -d "${BUILD_PATH}/android-build" \
+            -c "${BUILD_PATH}/package_info.conf" \
+            -c "${BUILD_PATH}/package_info_android.conf" \
+            -c "${PWD}/package_info_sdkbt.conf"
+        cp -rf "${BUILD_PATH}/android-build"/* "${PWD}/webcamoid-data"
+    done
+
+    cat << EOF > package_info_hide_arch.conf
+[Package]
+targetArch = any
+hideArch = true
+EOF
+
+    python3 DeployTools/deploy.py \
+        -s \
+        -d "${PWD}/webcamoid-data" \
+        -c "${PWD}/build/package_info.conf" \
+        -c "${PWD}/build/package_info_android.conf" \
+        -c "${PWD}/package_info_sdkbt.conf" \
+        -c "${PWD}/package_info_hide_arch.conf" \
+        -o "${PACKAGES_DIR}"
+fi
