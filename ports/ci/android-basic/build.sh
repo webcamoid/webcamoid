ea2df183c52e84f4d45be5fb888dfc4c54b7bf23
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2024-05-18 22:36:03 -0300

    Allow setting Qt source templates path.

--- ports/ci/android-basic/build.sh
+++ ports/ci/android-basic/build.sh
@@ -75,6 +75,7 @@ for arch_ in $(echo "${TARGET_ARCH}" | tr ":" "\n"); do
         -DANDROID_PLATFORM="${ANDROID_PLATFORM}" \
         -DANDROID_SDK_ROOT="${ANDROID_HOME}" \
         -DANDROID_NDK_ROOT="${ANDROID_NDK}" \
+        -DQT_INSTALL_SRC="${PWD}/build/Qt/${QTVER_ANDROID}/android_${arch_}/src" \
         -DQT_QMAKE_EXECUTABLE="${QMAKE_EXECUTABLE}" \
         -DLRELEASE_TOOL="${LRELEASE_TOOL}" \
         -DLUPDATE_TOOL="${LUPDATE_TOOL}" \
