016e2f310a69c106a7254767defeee472546e82c
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2024-08-11 13:10:43 -0300

    Fixing workflows 7.

--- ports/ci/linux-mingw/install_deps.sh
+++ ports/ci/linux-mingw/install_deps.sh
@@ -26,14 +26,38 @@ fi
 
 # Configure mirrors
 
-cat << EOF >> /etc/pacman.conf
+# cat << EOF >> /etc/pacman.conf
+#
+# [multilib]
+# Include = /etc/pacman.d/mirrorlist
+#
+# [ownstuff]
+# Server = https://ftp.f3l.de/~martchus/\$repo/os/\$arch
+# #Server = http://martchus.no-ip.biz/repo/arch/\$repo/os/\$arch
+# EOF
+# sed -i 's/Required DatabaseOptional/Never/g' /etc/pacman.conf
+#
+# cat << EOF >> /etc/pacman.d/mirrorlist
+#
+# Server = ${ARCH_ROOT_URL}/\$repo/os/\$arch
+# EOF
 
-[multilib]
-Include = /etc/pacman.d/mirrorlist
+sed -in '/^[core]/q;p' /etc/pacman.conf
+
+cat << EOF >> /etc/pacman.conf
 
 [ownstuff]
 Server = https://ftp.f3l.de/~martchus/\$repo/os/\$arch
-#Server = http://martchus.no-ip.biz/repo/arch/\$repo/os/\$arch
+Server = http://martchus.no-ip.biz/repo/arch/\$repo/os/\$arch
+
+[core]
+Include = /etc/pacman.d/mirrorlist
+
+[extra]
+Include = /etc/pacman.d/mirrorlist
+
+[multilib]
+Include = /etc/pacman.d/mirrorlist
 EOF
 sed -i 's/Required DatabaseOptional/Never/g' /etc/pacman.conf
 
