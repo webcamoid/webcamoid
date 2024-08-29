194a6295ce2c793a6ba9999e4641633cfef8395b
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2024-08-29 17:23:39 -0300

    Try to fix NSIS install.

--- ports/ci/linux-mingw/install_deps.sh
+++ ports/ci/linux-mingw/install_deps.sh
@@ -96,5 +96,7 @@ ${DOWNLOAD_CMD} "https://sourceforge.net/projects/nsis/files/NSIS%20${NSIS_VERSI
 if [ -e "${nsis}" ]; then
     export WINEPREFIX=/opt/.wine
 
-    xvfb-run --auto-servernum wine "${PWD}/${nsis}" /S
+    winPath=$(winepath -w "${PWD}/${nsis}")
+#    xvfb-run --auto-servernum wine "${winPath}" /S
+    wine "${winPath}" /S
 fi
