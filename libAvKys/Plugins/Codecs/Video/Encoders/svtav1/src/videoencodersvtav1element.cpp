4bc785ffdbd3833ca0803a772a300ddf62e9f772
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2026-03-29 21:01:44 -0300

    Added support for SVT-AV1 4.1.0 (issue #782).

--- libAvKys/Plugins/Codecs/Video/Encoders/svtav1/src/videoencodersvtav1element.cpp
+++ libAvKys/Plugins/Codecs/Video/Encoders/svtav1/src/videoencodersvtav1element.cpp
@@ -360,7 +360,14 @@ bool VideoEncoderSvtAv1ElementPrivate::init()
 #endif
 
     configs.rate_control_mode = SVT_AV1_RC_MODE_CBR;
-    configs.pred_structure = SVT_AV1_PRED_LOW_DELAY_B; // Otherwise CBR won't work
+
+    // Otherwise CBR won't work
+#if SVT_AV1_CHECK_VERSION(4, 1, 0)
+    configs.pred_structure = LOW_DELAY;
+#else
+    configs.pred_structure = SVT_AV1_PRED_LOW_DELAY_B;
+#endif
+
     configs.target_bit_rate = self->bitrate();
     configs.profile = eqFormat->profile;
     configs.color_primaries = EB_CICP_CP_UNSPECIFIED;
