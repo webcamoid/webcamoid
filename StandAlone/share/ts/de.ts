ebff87a8aafcb672558099d6a8d5c9efb50e9b6f
Ettore Atalan <atalanttore@googlemail.com>
2026-08-12 08:58:26 +0200

    Translated using Weblate (German)
    Currently translated at 99.7% (882 of 884 strings)

--- StandAlone/share/ts/de.ts
+++ StandAlone/share/ts/de.ts
@@ -886,12 +886,12 @@
     <message>
         <location filename="../qml/FirstRunFallbackDialog.qml" line="31"/>
         <source>Camera not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Kamera nicht gefunden</translation>
     </message>
     <message>
         <location filename="../qml/FirstRunFallbackDialog.qml" line="41"/>
         <source>It was not found any connected camera.&lt;br/&gt;Fallingback to the screen capture.</source>
-        <translation type="unfinished"></translation>
+        <translation>Es wurde keine angeschlossene Kamera gefunden.&lt;br/&gt;Es wird auf die Bildschirmaufnahme zurückgegriffen.</translation>
     </message>
 </context>
 <context>
@@ -2215,12 +2215,12 @@
     <message>
         <location filename="../qml/VideoInputAddCamera.qml" line="30"/>
         <source>Select the camera to add</source>
-        <translation type="unfinished"></translation>
+        <translation>Kamera zum Hinzufügen auswählen</translation>
     </message>
     <message>
         <location filename="../qml/VideoInputAddCamera.qml" line="55"/>
         <source>Camera</source>
-        <translation type="unfinished"></translation>
+        <translation>Kamera</translation>
     </message>
 </context>
 <context>
@@ -2332,7 +2332,7 @@
     <message>
         <location filename="../qml/VideoInputs.qml" line="68"/>
         <source>Add camera</source>
-        <translation type="unfinished"></translation>
+        <translation>Kamera hinzufügen</translation>
     </message>
     <message>
         <location filename="../qml/VideoInputs.qml" line="76"/>
@@ -3719,12 +3719,12 @@
     <message>
         <location filename="../../../libAvKys/ExtraPlugins/FaceDetect/share/qml/main.qml" line="473"/>
         <source>Pixel grid width</source>
-        <translation type="unfinished"></translation>
+        <translation>Breite des Pixelrasters</translation>
     </message>
     <message>
         <location filename="../../../libAvKys/ExtraPlugins/FaceDetect/share/qml/main.qml" line="490"/>
         <source>Pixel grid height</source>
-        <translation type="unfinished"></translation>
+        <translation>Höhe des Pixelrasters</translation>
     </message>
     <message>
         <location filename="../../../libAvKys/ExtraPlugins/FaceDetect/share/qml/main.qml" line="533"/>
