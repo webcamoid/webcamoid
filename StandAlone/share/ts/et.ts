152de8c9afbb11ee6f7e839b4c40cb1ba133ebe0
Priit Jõerüüt <jrthwlate@users.noreply.hosted.weblate.org>
2026-09-08 21:59:01 +0200

    Translated using Weblate (Estonian)
    Currently translated at 100.0% (927 of 927 strings)

--- StandAlone/share/ts/et.ts
+++ StandAlone/share/ts/et.ts
@@ -2608,12 +2608,12 @@
     <message>
         <location filename="../qml/VideoOutputAddEdit.qml" line="327"/>
         <source>Direct mode (fast mode)</source>
-        <translation type="unfinished"></translation>
+        <translation>Otserežiim (kiire)</translation>
     </message>
     <message>
         <location filename="../qml/VideoOutputAddEdit.qml" line="328"/>
         <source>Compatibility (slow mode)</source>
-        <translation type="unfinished"></translation>
+        <translation>Ühildusrežiim (aeglane)</translation>
     </message>
     <message>
         <location filename="../qml/VideoOutputAddEdit.qml" line="344"/>
